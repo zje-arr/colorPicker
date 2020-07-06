dfa2a43c453b7c5603fe98817b390ccaab7fd3ea
ZjeArr <jarylolz12@gmail.com>
2020-07-07 02:43:09 +0800

    port listening changes

--- app.js
+++ app.js
@@ -9,10 +9,10 @@ app.get('/', (req, res) => {
 	res.render('ColorPickerGame');
 });
 
-// app.listen(process.env.PORT, process.env.IP);
+app.listen(process.env.PORT, process.env.IP);
 
-app.listen('3000', () => {
-	hostname = '127.0.0.1';
-	port = 3000;
-	console.log(`YelpCamp running at http://${hostname}:${port}/`);
-});
+// app.listen('3000', () => {
+// 	hostname = '127.0.0.1';
+// 	port = 3000;
+// 	console.log(`YelpCamp running at http://${hostname}:${port}/`);
+// });
