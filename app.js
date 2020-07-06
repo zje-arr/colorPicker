a67ed196e384550dfd3b64cead223e48913c6206
ZjeArr <jarylolz12@gmail.com>
2020-07-07 02:42:19 +0800

    changes file name

--- app.js
+++ app.js
@@ -6,7 +6,13 @@ app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
 
 app.get('/', (req, res) => {
-	res.render('colorPickerGame');
+	res.render('ColorPickerGame');
 });
 
-app.listen(process.env.PORT, process.env.IP);
+// app.listen(process.env.PORT, process.env.IP);
+
+app.listen('3000', () => {
+	hostname = '127.0.0.1';
+	port = 3000;
+	console.log(`YelpCamp running at http://${hostname}:${port}/`);
+});
