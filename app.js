c1bb27db818899fbdf022178e521fcfeec37cdf9
Jaryl <jarylolz12@gmail.com>
2020-12-14 16:57:35 +0800

    initial commit for gcpBuild

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use('/public', express.static('public'));
+
+app.get('/', (req, res) => {
+	res.render('ColorPickerGame');
+});
+
+app.listen(process.env.PORT, process.env.IP);
+
+// app.listen('3000', () => {
+// 	hostname = '127.0.0.1';
+// 	port = 3000;
+// 	console.log(`YelpCamp running at http://${hostname}:${port}/`);
+// });
