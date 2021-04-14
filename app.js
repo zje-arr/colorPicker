9a9279b1821af2d3794873e7202275f3cfbfbc8b
Jaryl <jarylolz12@gmail.com>
2021-04-14 23:33:44 +0800

    set the port to localhost:8383 golang

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ app.get('/', (req, res) => {
 
 // app.listen(process.env.PORT, process.env.IP);
 
-app.listen('8080', () => {
+app.listen('8383', () => {
 	hostname = '127.0.0.1';
-	port = 8080;
+	port = 8383;
 	console.log(`YelpCamp running at http://${hostname}:${port}/`);
 });
